5731a3a7708a24359fdd65f2e12a3232496e2a89
berryzhuhpe <109754384+berryzhuhpe@users.noreply.github.com>
2022-07-28 15:21:33 -0400

    new feature 1

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,11 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test Output");
+        feature1();
+    }
+
+    public static void feature1() {
+        System.out.println("feature-1");
     }
 
 }
